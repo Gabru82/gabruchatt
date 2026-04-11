55d69f24582f6be4b7109f9815ce896907a80ec7
Gabru82 <rc82398266@gmail.com>
2026-04-11 15:04:55 +0530

    complete9

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ app.use(express.json({ limit: "100mb" }));
 // ================= FIREBASE ADMIN =================
 const admin = require("firebase-admin");
 // Ensure you have a serviceAccountKey.json in your project root or use env variables
-const serviceAccount = require("./serviceAccountKey.json"); 
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 app.use(cors());
